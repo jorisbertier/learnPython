7ed25c5e848e1f1c9f8a2d60d5d31cc1a86f0fc0
Joris Bertier <joris.bertier@gmail.com>
2024-12-17 15:46:12 -0600

    beautifulSoup exercice

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
-import operations
+from bs4 import BeautifulSoup
 
-print(operations.addition(1, 1))
-print(operations.multiplication(2, 2))
+# Extraction des informations souhaitées avec Beautiful Soup
+with open("index.html", "r") as file:
+    soup = BeautifulSoup(file, "html.parser")
+
+# Extraction du titre de la page
+title = soup.title.string
+print("Titre de la page:", title)
+
+# Extraction du texte de la balise h1
+h1_text = soup.find("h1").string
+print("Texte de la balise h1:", h1_text)
+
+# Dictionnaire pour stocker les produits
+all_products = dict()
+
+# Extraction des noms et des prix des produits dans la liste
+products = soup.find_all("li")
+for product in products:
+    name = product.find("h2").string
+    price_str = product.find("p", class_="price").string
+    # On sépare la chaine avec " " en liste de mots
+    price_list = price_str.split(" ")
+    # On récupère le prix (= deuxième mot)
+    all_products[name] = {"prix": price_list[1]}
+
+# Extraction des descriptions des produits dans la liste
+descriptions_list = []
+for product in products:
+    # La description eest le dernier élément de la liste des paragraphes
+    description = product.find_all("p")[-1].string
+    all_products[name]["description"] = description
+
+# Affichage des informations extraites
+print("Produits:", all_products)
