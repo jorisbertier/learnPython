d24d7a563723574a71983d70a14cd48f769d4239
Joris Bertier <joris.bertier@gmail.com>
2024-12-18 11:11:18 -0600

    add requirements.txt & create virtual host

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import boto3
-import numpy
 import requests
-from flask import Flask
-
-
-print("Congratulations. You have installed the right packages and your script has executed successfully!")
+r = requests.get('http://www.example.com')
+print(r.status_code)
