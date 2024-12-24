fe0332fcf2b6ce682a1acdadf0f4d26242031824
Joris Bertier <joris.bertier@gmail.com>
2024-12-24 16:32:17 -0600

    readme

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-import requests
-r = requests.get('http://www.example.com')
-print(r.status_code)
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv('notes.csv')
+moyenne_matiere = data.groupby('matiere').agg({'note' : ['mean']})
+moyenne_par_eleve = data.groupby('nom').agg({'note' : ['mean']})
+print(moyenne_par_eleve)
